892995153ffd53768b131477d708de6902be35bd
Adriana Eka Prayudha <radenadriep@gmail.com>
2024-09-30 17:28:21 +0700

    fix: step 6

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,12 +1,30 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
-const links = [
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * A component that renders a list of navigation links.
+ *
+ * Each link is rendered as a styled `Link` component from Next.js.
+ * The `pathname` is used to determine which link is currently active,
+ * and the active link is rendered with a blue background and white text.
+ *
+ * The links are rendered in a horizontal row on large screens and a
+ * vertical column on small screens.
+ *
+ * @returns A list of links, each with an icon and a label.
+ */
+/******  477756f5-e053-472b-8980-b1db1980b0e2  *******/const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
@@ -17,19 +35,26 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              }
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
